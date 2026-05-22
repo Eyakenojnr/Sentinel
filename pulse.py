4f72d4906d2fefb32d1d8b23152d457586babb78
Uduak Umanah <eyakenojnr@gmail.com>
2026-05-22 21:49:03 +0100

    Removed exceptions handling 404 and 500 because this is an Uptime monitor and that data is important

--- pulse.py
+++ pulse.py
@@ -10,15 +10,9 @@ import time
 url = input("Enter url: ")
 
 try:
-    start_time = time.perf_counter()
     r = requests.get(url, timeout=1, verify=True)
-    r.raise_for_status()
-    end_time = time.perf_counter()
-    duration_ms = (end_time - start_time) * 1000
+    duration_ms = r.elapsed.total_seconds() * 1000
     print(f'The url {url} with status code [{r.status_code}] took {duration_ms}ms to respond.')
-except requests.exceptions.HTTPError as errh:
-    print("HTTP Error")
-    print(errh.args[0])
 except requests.exceptions.ReadTimeout as errrt:
     print("Time out")
 except requests.exceptions.MissingSchema as errmiss:
