8fb5a6aeab01eae479aadb2fc56085c11a3842d9
Uduak Umanah <eyakenojnr@gmail.com>
2026-05-27 21:52:41 +0100

    Sort out problem of repeated headers in csv file

--- pulse.py
+++ pulse.py
@@ -6,8 +6,9 @@
 """
 import requests
 import time
-from datetime import datetime, timezone
 import csv
+import os
+from datetime import datetime, timezone
 
 url = input("Enter url: ")
 output_file = 'metrics.csv'
@@ -17,7 +18,7 @@ def ping_url(url):
     Args:
         url (string): website url
     Returns:
-        A dictionary
+        A dictionary containing timestamp, url, status code, time (in ms), and error
     """
     current_time = datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S")
     
@@ -33,10 +34,18 @@ def ping_url(url):
     
     return {"timestamp": current_time, "url": url, "status_code": r.status_code, "time_ms": duration_ms, "error": None}
 
+
+# Check if file exists before writing to it, this prevent writing  CSV header multiple times when appending to the file
+file_exists = os.path.isfile(output_file)
+needs_header = not file_exists or os.path.getsize(output_file) == 0  # Header is only needed if file doesn't exist OR if it is empty (0 bytes)
+
 with open(output_file, 'a', newline='') as csvfile:
     field_names = ['timestamp', 'url', 'status_code', 'time_ms', 'error']
     writer = csv.DictWriter(csvfile, fieldnames=field_names)
-    writer.writeheader()
+    
+    if needs_header:
+        writer.writeheader()
+        csvfile.flush()  # Ensures header is written immediately
     
     print(f"Monitoring {url}... Press Ctrl+C to stop.")
     
@@ -44,8 +53,10 @@ with open(output_file, 'a', newline='') as csvfile:
         while True:
             result = ping_url(url)
             writer.writerow(result)
-            csvfile.flush()  # Force Py to write to file immediately to prevent data loss
-            print(f"{result['timestamp']} - {result['status_code']} - {result['time_ms']:.2f}ms")
+            csvfile.flush()  # Force Python to write to file immediately to prevent data loss
+            # Check if data exists before printing to console
+            time_str = f"{result['time_ms']:.2f}ms" if result['time_ms'] else "N/A"
+            print(f"{result['timestamp']} - {result['status_code']} - {time_str}")
             time.sleep(5)  # Pause for 5 secs
     except KeyboardInterrupt:
         print("\nMonitoring stopped.")
