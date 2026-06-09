7299446be891936db7b1d62a377843b979ee6661
Uduak Umanah <eyakenojnr@gmail.com>
2026-06-09 22:54:17 +0100

    OOP refactor

--- pulse.py
+++ pulse.py
@@ -11,82 +11,107 @@ from datetime import datetime, timezone
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
 
-urls = [
+URLS = [
     "https://google.com",
     "https://httpstat.us/500",
     "http://this-is-a-bad-url.com",
     "https://github.com"
 ]
-db_name = 'metrics.db'
-interval = 5
-# Initialize database
-conn = sqlite3.connect(db_name, check_same_thread=False)
-cursor = conn.cursor()
-# Create table if it doesn't exist
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS pings (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        timestamp TEXT,
-        url TEXT,
-        status_code INTEGER,
-        time_ms REAL,
-        error TEXT
-    )
-''')
-conn.commit()
+INTERVAL = 5
+DB_NAME = "metrics.db"
 
-def ping_url(url):
-    """Function that pings a website.
-    Args:
-        url (string): website url
-    Returns:
-        A dictionary containing timestamp, url, status code, time (in ms), and error
-    """
-    current_time = datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S")
+
+class Database:
+    def __init__(self, db_name):
+        """Connects to SQLite and creates a table."""
+        self.conn = sqlite3.connect(db_name, check_same_thread=False)
+        self.cursor = self.conn.cursor()
+        
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS pings (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                timestamp TEXT,
+                url TEXT,
+                status_code INTEGER,
+                time_ms REAL,
+                error TEXT
+            )
+        """)
+        self.conn.commit()
+        
+    def insert_pings(self, ping_results):
+        """Batch inserts a list of dictionaries."""
+        query = """INSERT INTO pings (timestamp, url, status_code, time_ms, error)
+                    VALUES (:timestamp, :url, :status_code, :time_ms, :error)"""
+        self.cursor.executemany(query, ping_results)
+        self.conn.commit()
+        
+    def close(self):
+        self.conn.close()
+        
+        
+class SentinelMonitor:
+    def __init__(self, urls, db_name):
+        """Instantiate Database class and ThreadPoolExecutor."""
+        self.db = Database(db_name)
+        self.urls = urls
+                
+                
+    def _ping_url(self, url):
+        """Private method to ping a single URL."""
+        current_time = datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S")
+        
+        try:
+            r = requests.get(url, timeout=4)
+            return {
+                "timestamp": current_time, "url": url,
+                "status_code": r.status_code,
+                "time_ms": r.elapsed.total_seconds() * 1000,
+                "error": None
+            }
+        except requests.exceptions.ReadTimeout as errt:
+            return {"timestamp": current_time, "url": url, "status_code": None, "time_ms": None, "error": "Time out"}
+        except requests.exceptions.MissingSchema as errmiss:
+            return {"timestamp": current_time, "url": url, "status_code": None, "time_ms": None, "error": "Missing Schema: check URL and try again"}
+        except requests.exceptions.ConnectionError as conerr:
+            return {"timestamp": current_time, "url": url, "status_code": None, "time_ms": None, "error": "Connection error"}
     
-    try:
-        r = requests.get(url, timeout=4)
-        duration_ms = r.elapsed.total_seconds() * 1000
-    except requests.exceptions.ReadTimeout as errrt:
-        return {"timestamp": current_time, "url": url, "status_code": None, "time_ms": None, "error": "Time out"}
-    except requests.exceptions.MissingSchema as errmiss:
-        return {"timestamp": current_time, "url": url, "status_code": None, "time_ms": None, "error": "Missing schema: check URL and try again"}
-    except requests.exceptions.ConnectionError as conerr:
-        return {"timestamp": current_time, "url": url, "status_code": None, "time_ms": None, "error": "Connection error"}
     
-    return {"timestamp": current_time, "url": url, "status_code": r.status_code, "time_ms": duration_ms, "error": None}
+    
+    def run(self):
+        print(f"Sentinel active. Monitoring {len(self.urls)} targets...")
+        
+        with ThreadPoolExecutor(max_workers=len(self.urls)) as executor:
+            try:
+                while True:
+                    start_tick = time.perf_counter()  # Record precise starting time
+                    batch_results = []
+                    
+                    # Start concurrent pings
+                    future_to_url = {executor.submit(self._ping_url, url): url for url in self.urls}
 
+                    # Collect results as they finnish
+                    for future in as_completed(future_to_url):
+                        result = future.result()  # Converts future object to a dictionary
+                        batch_results.append(result)
+                        
+                        # Live feedback to terminal
+                        status = result['status_code'] or "ERR"
+                        print(f"[{result['timestamp']}] {result['url']}: {status}")
+                    
+                    # Batch insert everything at once
+                    self.db.insert_pings(batch_results)
+                    
+                    # Handle time drift
+                    elapsed = time.perf_counter() - start_tick
+                    sleep_time = max(0, INTERVAL - elapsed)
+                    time.sleep(sleep_time)
+            except KeyboardInterrupt:
+                print(f"\nMonitoring stopped by user.")
+            finally:
+                self.db.close()
+                
 
-print(f"Sentinel active!\nMonitoring {urls}... \nPress Ctrl+C to stop.")
-
-# Using ThreadPoolExecutor to ping all URLs at once
-with ThreadPoolExecutor(max_workers=len(urls)) as executor:
-    try:
-        while True:
-            # Record precise start time
-            start_tick = time.perf_counter()
-            
-            # Map the function to the URLs
-            future_to_url = {executor.submit(ping_url, url): url for url in urls}
-            
-            # Process results as they finish
-            for future in as_completed(future_to_url):
-                data = future.result()
-                # Insert into SQLite    
-                query = '''INSERT INTO pings (timestamp, url, status_code, time_ms, error)
-                            VALUES (:timestamp, :url, :status_code, :time_ms, :error)'''
-                cursor.execute(query, data)
-            
-            # Batch commit: 1 disk write per interval
-            conn.commit()
-            
-            # Compensation for time drift
-            elapsed = time.perf_counter() - start_tick
-            sleep_time = max(0, interval - elapsed)
-            
-            print(f"Batch completed in {elapsed:.2f}s. Sleeping for {sleep_time:.2f}s")
-            time.sleep(sleep_time)
-    except KeyboardInterrupt:
-        print("\nMonitoring stopped.")
-    finally:
-        conn.close()
+if __name__ == "__main__":
+    monitor = SentinelMonitor(URLS, DB_NAME)
+    monitor.run()
