dd9beed16a891371753d1ce46bd816c063e919af
Uduak Umanah <eyakenojnr@gmail.com>
2026-06-13 18:37:01 +0100

    Enable CORS

--- api.py
+++ api.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 import sqlite3
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all frontends to connect
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
