df8718a07c3e52fa2377f6cedc0111e585c960c6
Uduak Umanah <eyakenojnr@gmail.com>
2026-06-13 16:56:22 +0100

    Updated api

--- api.py
+++ api.py
@@ -11,8 +11,9 @@ def read_root():
 
 @app.get("/status")
 def status():
+    """Get the most recent result for each URL."""
     conn = sqlite3.connect("metrics.db")
-    conn.row_factory = sqlite3.Row
+    conn.row_factory = sqlite3.Row  # Makes database rows act like dictionaries
     try:
         cursor = conn.cursor()
         query = """SELECT url, status_code, time_ms, error, MAX(timestamp) 
@@ -26,3 +27,21 @@ def status():
         conn.close()
         
     return list_of_dicts #json_data
+
+@app.get("/history")
+def history(url: str):
+    """Query the database for the last 10 pings."""
+    conn = sqlite3.connect('metrics.db')
+    conn.row_factory = sqlite3.Row
+    try:
+        cursor = conn.cursor()
+        query = """SELECT * FROM pings WHERE url = ?
+                    ORDER BY timestamp DESC
+                    LIMIT 10"""
+        cursor.execute(query, (url,))
+        rows = cursor.fetchall()
+        list_of_dicts = [dict(row) for row in rows]
+    finally:
+        conn.close()
+    
+    return list_of_dicts
